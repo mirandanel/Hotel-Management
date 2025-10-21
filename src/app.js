9340a5b7c25f27e0150b53c824cb46d185ade875
mirandanel <miranda.nelia1803@gmail.com>
2025-10-21 08:34:02 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -45,8 +45,8 @@ app.use('/api/bookings', bookingRoutes);
 // Error handler (must be last)
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+app.get('/', (req, res) => {
+    res.send(`Server is running`);
 });
+
+module.exports = app;
